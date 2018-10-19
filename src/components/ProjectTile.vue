8bf51c3b9bcf7bd6bf0907b8f016efd4bfa1237c
Wes <wes977@gmail.com>
2018-10-18 23:32:56 -0400

    Got the on hover effect working now so when you hover over a tile it will go green and display the data you want TODO is when you hover off you can see the box shrink fix that shit! ASAP

--- src/components/ProjectTile.vue
+++ src/components/ProjectTile.vue
@@ -1,26 +1,28 @@
 <template>
     <v-flex d-flex xs6 sm6 md3 lg3 v-if="this.tileSize == 1 ">
-        <v-card :hover="true" tile flat height="100%" >
-            <div class="DisplayCardContainer">
-            <div class="InfoCard">This is a TEST</div>
-            <div class="DisplayCard">
-                <img src="../assets/old-milwaukee.png" alt="Smiley face" height="auto" width="100%"> 
-            </div>
-            </div>
+        <v-card :hover="true" tile flat height="100%" class="ProjectBoxContainer">
+
+                <img src="../assets/old-milwaukee.png" alt="Avatar" class="image" style="width:100%">
+                <div class="middle">
+                    <div class="text">Cold Milwaukee</div>
+                </div>
+
         </v-card>
     </v-flex>
         <v-flex d-flex xs12 sm12 md6 lg6 v-else-if="this.tileSize == 2">
-        <v-card :hover="true" tile flat >
-            <div class="DisplayCard">
-                <img src="../assets/mobile_About.jpg" alt="Smiley face" height="auto" width="100%"> 
+        <v-card :hover="true" tile flat class="ProjectBoxContainer">
+                <img src="../assets/mobile_About.jpg" alt="Avatar" class="image" style="width:100%">
+                <div class="middle">
+                    <div class="text">Cold Milwaukee</div>
             </div>
         </v-card>
     </v-flex>
             <v-flex d-flex xs6 sm6 md3 lg3 v-else-if="this.tileSize == 3">
-        <v-card :hover="true" tile flat >
-            <div class="DisplayCard">
-                <img src="../assets/OldMillCan.png" alt="Smiley face" height="auto" width="100%"> 
-            </div>
+        <v-card :hover="true" tile flat class="ProjectBoxContainer">
+                            <img src="../assets/OldMillCan.png" alt="Avatar" class="image" style="width:100%">
+                <div class="middle">
+                    <div class="text">Cold Milwaukee</div>
+                </div>
         </v-card>
     </v-flex>
 </template>
@@ -37,9 +39,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.DisplayCardContainer {
-    
-}
 
 .DisplayCard,
 .InfoCard {
@@ -50,22 +49,27 @@ export default {
   left: 0;
 }
 .InfoCard {
-    opacity: 0.5;
-  z-index: 10;
-  width: auto;
+    opacity: 0;
+  width: 100%;
   height: 100%;
   display: none;
+  z-index: 30;
+  background-color: transparent; 
+      -moz-transition: opacity .75s ease-in-out;
+    -o-transition: opacity .75s ease-in-out;
+    -webkit-transition: opacity .75s ease-in-out;
+    transition: opacity .75s ease-in-out;
 }
 
 .DisplayCard {
-    opacity: 1;
-    height: 100%;
-    max-height: 250px;
+
+
    background: #fff;
-    -moz-transition: all .2s ease-in;
-    -o-transition: all .2s ease-in;
-    -webkit-transition: all .2s ease-in;
-    transition: all .2s ease-in;
+    transition: thing 5s ;
+    -moz-transition: thing 5s ;
+    -o-transition: thing 5s ;
+    -webkit-transition: thing 5s ;
+    
     background: #fff;
 
 }
@@ -75,17 +79,59 @@ export default {
     max-height: 250px;
 }
 .DisplayCard:hover {
-    background: rgb(231, 7, 7) !important;
-
+        z-index: 0;
+        opacity: 0.25;
+}
+.InfoCard:hover {
+opacity: 1;
 }
 
-@keyframes highlight {
- 	0% {
- 		background: red
- 	}
- 	100% {
- 		background: red;
- 	}
+
+.ProjectBoxContainer {
+    position: relative;
+    width: 100%;
+        height: 100%;
+    max-height: 250px;
 }
+
+.image {
+  opacity: 1;
+  display: block;
+  width: 100%;
+  height: auto;
+  transition: .5s ease;
+  backface-visibility: hidden;
+      max-height: 250px;
+}
+
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  text-align: center;
+}
+
+div.ProjectBoxContainer:hover img.image {
+  opacity: 0.3;
+}
+
+.ProjectBoxContainer:hover .middle {
+  opacity: 1;
+    width: 100%;
+  height: 100%;
+}
+
+.text {
+  background-color: #4CAF50;
+  color: white;
+  font-size: 16px;
+  padding: 16px 32px;
+height: 100%;
+opacity: 0.6;
+}
+
 
 </style>
